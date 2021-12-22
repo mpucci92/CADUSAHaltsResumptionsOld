eaad79322ddc4a23efde4d0a907e440a40bbe65a
mpucci92 <42786192+mpucci92@users.noreply.github.com>
2021-12-21 23:46:28 -0500

    Update UnitedStatesHaltsResumptions.py

--- UnitedStatesHaltsResumptions.py
+++ UnitedStatesHaltsResumptions.py
@@ -1,12 +1,17 @@
 from datetime import date
 import pandas as pd
+import time
+from datetime import datetime
+from pytz import timezone
+tz = timezone('EST')
+currentDate = (str(datetime.now(tz)).split(" ")[0])
 
 def downloadAmericanHalts(path_to_save):
-    currentDate = date.today()
     nyse_nasdaq_url = f'https://www.nyse.com/api/trade-halts/historical/download?symbol=&reason=&sourceExchange=&haltDateFrom={currentDate}&haltDateTo='
     df = pd.read_csv(nyse_nasdaq_url)
     df.to_csv(path_to_save, index=False)
-
+    print(df)
+    print(nyse_nasdaq_url)
 
 def cleanNamesAmerican(df):
     cleanNames = []
